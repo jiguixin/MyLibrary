84befb8e781e4c6b82953617b816c29e24ece601
jiguixin <jiguixin_1008@qq.com>
2013-07-07 23:54:14 +0800

    增加unicode转汉字

--- Infrastructure.Crosscutting/Utility/CommomHelper/TextHelper.cs
+++ Infrastructure.Crosscutting/Utility/CommomHelper/TextHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using Infrastructure.Crosscutting.Declaration;
@@ -225,5 +226,37 @@ namespace Infrastructure.Crosscutting.Utility.CommomHelper
             }
             return lstResult;
         }
+
+        /// <summary>  
+        /// 汉字转换为Unicode编码  
+        /// </summary>  
+        /// <param name="str">要编码的汉字字符串</param>  
+        /// <returns>Unicode编码的的字符串</returns>  
+        public static string ToUnicode(string str)
+        {
+            byte[] bts = Encoding.Unicode.GetBytes(str);
+            string r = "";
+            for (int i = 0; i < bts.Length; i += 2) r += "\\u" + bts[i + 1].ToString("x").PadLeft(2, '0') + bts[i].ToString("x").PadLeft(2, '0');
+            return r;
+        }
+
+        /// <summary>  
+        /// 将Unicode编码转换为汉字字符串  
+        /// </summary>  
+        /// <param name="str">Unicode编码字符串</param>  
+        /// <returns>汉字字符串</returns>  
+        public static string ToGB2312(string str)
+        {  
+            string r = "";
+            MatchCollection mc = Regex.Matches(str, @"\\u([\w]{2})([\w]{2})", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            byte[] bts = new byte[2];
+            foreach (Match m in mc)
+            {
+                bts[0] = (byte)int.Parse(m.Groups[2].Value, NumberStyles.HexNumber);
+                bts[1] = (byte)int.Parse(m.Groups[1].Value, NumberStyles.HexNumber);
+                r += Encoding.Unicode.GetString(bts);
+            }
+            return r;
+        }  
     }
 }
