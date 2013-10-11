fdbf8b8ece899093016e8d05f367fff062daedbf
jiguixin <jiguixin_1008@qq.com>
2013-10-11 15:20:20 +0800

    修复MVC 返回JSON的时间格式错误

---
+++ Infrastructure.CrossCutting.Web.Mvc/Core/BaseController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.CrossCutting.Web.Mvc.Core
+{
+    using System.Web.Mvc;
+
+    using Infrastructure.CrossCutting.Web.Mvc.Core.Json;
+
+    public class BaseController : Controller
+    {
+        
+        /// <summary>
+        /// 返回JsonResult
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="contentType">内容类型</param>
+        /// <param name="contentEncoding">内容编码</param>
+        /// <param name="behavior">行为</param>
+        /// <returns>JsonReuslt</returns>
+        protected override JsonResult Json(object data, string contentType, System.Text.Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior,
+                FormateStr = "yyyy-MM-dd HH:mm:ss"
+            };
+        }
+
+        /// <summary>
+        /// 返回JsonResult.24         
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="behavior">行为</param>
+        /// <param name="format">json中dateTime类型的格式</param>
+        /// <returns>Json</returns>
+        protected JsonResult MyJson(object data, JsonRequestBehavior behavior, string format)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                JsonRequestBehavior = behavior,
+                FormateStr = format
+            };
+        }
+
+        /// <summary>
+        /// 返回JsonResult42         
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="format">数据格式</param>
+        /// <returns>Json</returns>
+        protected JsonResult MyJson(object data, string format)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                FormateStr = format
+            };
+        }
+    }
+}
---
+++ Infrastructure.CrossCutting.Web.Mvc/Core/Json/CustomJsonResult.cs
@@ -0,0 +1,88 @@
+﻿/*
+ *名称：CustomJsonResult
+ *功能：
+ *创建人：吉桂昕
+ *创建时间：2013-10-11 15:11:05
+ *修改时间：
+ *备注：
+ */
+
+using System;
+
+namespace Infrastructure.CrossCutting.Web.Mvc.Core.Json
+{
+    using System.Text.RegularExpressions;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Script.Serialization;
+
+    /// <summary>
+    /// 自定义Json视图
+    /// </summary>
+    public class CustomJsonResult : JsonResult
+    {
+        /// <summary>
+        /// 格式化字符串
+        /// </summary>
+        public string FormateStr
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 重写执行视图
+        /// </summary>
+        /// <param name="context">上下文</param>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            if (string.IsNullOrEmpty(this.ContentType))
+            {
+                response.ContentType = this.ContentType;
+            }
+            else
+            {
+                response.ContentType = "application/json";
+            }
+
+            if (this.ContentEncoding != null)
+            {
+                response.ContentEncoding = this.ContentEncoding;
+            }
+
+            if (this.Data != null)
+            {
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                string jsonString = jss.Serialize(Data);
+                string p = @"\\/Date\((\d+)\)\\/";
+                MatchEvaluator matchEvaluator = new MatchEvaluator(this.ConvertJsonDateToDateString);
+                Regex reg = new Regex(p);
+                jsonString = reg.Replace(jsonString, matchEvaluator);
+
+                response.Write(jsonString);
+            }
+        }
+
+        /// <summary>  
+        /// 将Json序列化的时间由/Date(1294499956278)转为字符串 .
+        /// </summary>  
+        /// <param name="m">正则匹配</param>
+        /// <returns>格式化后的字符串</returns>
+        private string ConvertJsonDateToDateString(Match m)
+        {
+            string result = string.Empty;
+            DateTime dt = new DateTime(1970, 1, 1);
+            dt = dt.AddMilliseconds(long.Parse(m.Groups[1].Value));
+            dt = dt.ToLocalTime();
+            result = dt.ToString(FormateStr);
+            return result;
+        }
+    }
+}
